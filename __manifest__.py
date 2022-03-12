2d46e158336d467ebf668fecbc4bcdbc76fbc3f7
Ajay Mahato <ajjayymahato@gmail.com>
2022-03-12 19:13:54 +0530

    disable vat validation

--- __manifest__.py
+++ __manifest__.py
@@ -17,10 +17,10 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '12',
+    'version': '12.4',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','crm'],
+    'depends': ['base','crm', 'base_vat'],
 
     # always loaded
     'data': [
--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+#from odoo.addons.base_vat.models.res_partner import ResPartner
 
 class PartnerInherit(models.Model):
     _name = 'res.partner'
@@ -9,3 +9,15 @@ class PartnerInherit(models.Model):
 
     in_beta = fields.Boolean(default=False, string="In Beta")
 
+    def __init__(self):
+        self.env.context = dict(self.env.context)
+        self.env.context.update({
+            'no_vat_validation': True,
+        })
+
+    #@api.constrains('vat', 'country_id')
+    #def check_vat_extended(self):
+    #    raise Exception("Vikas")
+    #    return True
+
+    #ResPartner.check_vat = check_vat_extended
