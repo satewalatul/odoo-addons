8ff670398daf301fd6b6950f3f6c3f97f17098c5
Ajay Mahato <ajjayymahato@gmail.com>
2022-04-16 14:04:15 +0530

    Fixes

--- models/opportunity_inherit.py
+++ models/opportunity_inherit.py
@@ -10,9 +10,4 @@ class CrmLead(models.Model):
     _name = 'crm.lead'
     _inherit = 'crm.lead'
 
-    in_beta = fields.Boolean(default=False, string="In Beta")
     job_order = fields.Char(string="Job Order")
-
-    def button_function(self):
-        self.in_beta = True
-        self.partner_id.in_beta = True
